85200dcacc249b61c732f1b15166cef5f4a64ee9
fufex3934 <fufawakgari174@gmail.com>
2025-04-17 13:24:36 +0300

    contact

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+const Contact = () => {
+  return (
+    <div>
+        <h1>Contact Page</h1>
+        <Link href={'/about'}>About</Link>
+    </div>
+  )
+}
+
+export default Contact
